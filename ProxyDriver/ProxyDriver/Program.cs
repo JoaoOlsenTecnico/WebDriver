d5d32f28b1f7a3124280f3aeb99c2a8038f03c46
João <joao.vitor@inovaebiz.com.br>
2019-05-29 11:34:45 -0300

    Commit 1.1 | 29/05/2019 - Proxy para Firefox.

--- ProxyDriver/ProxyDriver/Program.cs
+++ ProxyDriver/ProxyDriver/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using System;
 
 namespace ProxyDriver
@@ -8,19 +9,26 @@ namespace ProxyDriver
     {
         static void Main(string[] args)
         {
+            ProxyDriver();
+        }
+
+        public static void ProxyDriver()
+        {
             // Adiciona argumento que remove corpo do navegador(Deixando apenas visual pelo console)
-            //options.AddArguments("--headless");
+            // options.AddArguments("--headless");
             var options = new ChromeOptions();
             var proxy = new Proxy();
             proxy.Kind = ProxyKind.Manual;
             proxy.IsAutoDetect = false;
-            proxy.HttpProxy = 
-            proxy.SslProxy = "1.20.101.221:55707";
+            proxy.HttpProxy = proxy.SslProxy = "1.20.101.221:55707";
             options.Proxy = proxy;
             options.AddArgument("ignore-certificate-errors");
             // Inicia o navegador
-            var chromeDriver = new ChromeDriver(options);
-            // Acessando o painel de admin do Magento
+            //var firefoxDriver = new FirefoxDriver(@"C:\chromeDriver\", options_firefox);
+            //var firefoxDriver_2 = new FirefoxDriver(@"C:\chromeDriver\teste\", options_firefox);
+            var chromeDriver = new ChromeDriver(@"C:\chromeDriver\teste\" ,options);
+            // Acessando o site
+            //firefoxDriver.Navigate().GoToUrl("https://www.meuip.com.br/");
             chromeDriver.Navigate().GoToUrl("https://www.meuip.com.br/");
         }
     }
