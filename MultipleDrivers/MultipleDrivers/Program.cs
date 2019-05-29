62914448c8972a61556d0eae6982c9b1ded727bf
João <joao.vitor@inovaebiz.com.br>
2019-05-29 13:59:54 -0300

    Commit 1.2 | 29/05/2019 - Crawler de IP's + TXT com 2200 Proxys.

---
+++ MultipleDrivers/MultipleDrivers/Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace ProxyDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void ProxyDriver()
+        {
+            // Adiciona argumento que remove corpo do navegador(Deixando apenas visual pelo console)
+            // options.AddArguments("--headless");
+            var options = new ChromeOptions();
+            var options_firefox = new FirefoxOptions();
+            var proxy = new Proxy();
+            proxy.Kind = ProxyKind.Manual;
+            proxy.IsAutoDetect = false;
+            proxy.HttpProxy = proxy.SslProxy = "1.20.101.221:55707";
+            options_firefox.Proxy = proxy;
+            options.Proxy = proxy;
+            options_firefox.AddArgument("ignore-certificate-errors");
+            options.AddArgument("ignore-certificate-errors");
+            // Inicia o navegador
+            var firefoxDriver = new FirefoxDriver(options_firefox);
+            //var chromeDriver = new ChromeDriver(options);
+            // Acessando o site
+            firefoxDriver.Navigate().GoToUrl("https://www.meuip.com.br/");
+            //chromeDriver.Navigate().GoToUrl("https://www.meuip.com.br/");
+        }
+    }
+}
--- ProxyDriver/ProxyDriver/Program.cs
+++ ProxyDriver/ProxyDriver/Program.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
+using System.IO;
 
 namespace ProxyDriver
 {
@@ -9,7 +10,53 @@ namespace ProxyDriver
     {
         static void Main(string[] args)
         {
-            ProxyDriver();
+            PegandoIPs();
+        }
+
+        public static void PegandoIPs()
+        {
+            var options = new ChromeOptions();
+            //options.AddArguments("--headless");
+            //options.AddArguments("--disable-gpu");
+            //var proxy = new Proxy();
+            //proxy.Kind = ProxyKind.Manual;
+            //proxy.IsAutoDetect = false;
+            //proxy.HttpProxy = proxy.SslProxy = "1.20.101.221:55707";
+            //options.Proxy = proxy;
+            //options.AddArgument("ignore-certificate-errors");
+            var chromeDriver = new ChromeDriver(@"C:\chromeDriver\teste\", options);
+            var numero = 0;
+            StreamWriter sw = new StreamWriter(@"C:\chromeDriver\ip.txt");
+            chromeDriver.Navigate().GoToUrl("https://hidemyna.me/en/proxy-list/?maxtime=1000");
+            System.Threading.Thread.Sleep(10000);
+            var campos_proxy = chromeDriver.FindElementsByTagName("table")[0].FindElements(By.TagName("tr"));
+            Console.WriteLine(campos_proxy[0].Text);
+                //.FindElements(By.TagName("tr"));
+            for (var x = 0; x < campos_proxy.Count; x++)
+            { 
+                try
+                {
+                    var ip = campos_proxy[x].FindElements(By.TagName("td"))[0].Text;
+                    var porta = campos_proxy[x].FindElements(By.TagName("td"))[1].Text;
+                    Console.WriteLine(ip + ":" + porta);
+                    sw.WriteLine(ip + ":" + porta);
+                }
+                catch(Exception e) {}
+                if(x == campos_proxy.Count-1)
+                {
+                    if(numero == 1984)
+                    {
+                        sw.Close();
+                        break;
+                    }
+                    numero += 64;
+                    chromeDriver.Navigate().GoToUrl("https://hidemyna.me/en/proxy-list/?maxtime=1000&start="+ numero +"#list"); 
+                    campos_proxy = chromeDriver.FindElementsByTagName("table")[0].FindElements(By.TagName("tr"));
+                    x = 0;
+                }
+               
+            }
+            
         }
 
         public static void ProxyDriver()
@@ -22,11 +69,13 @@ namespace ProxyDriver
             proxy.IsAutoDetect = false;
             proxy.HttpProxy = proxy.SslProxy = "1.20.101.221:55707";
             options.Proxy = proxy;
+            
             options.AddArgument("ignore-certificate-errors");
             // Inicia o navegador
             //var firefoxDriver = new FirefoxDriver(@"C:\chromeDriver\", options_firefox);
             //var firefoxDriver_2 = new FirefoxDriver(@"C:\chromeDriver\teste\", options_firefox);
             var chromeDriver = new ChromeDriver(@"C:\chromeDriver\teste\" ,options);
+
             // Acessando o site
             //firefoxDriver.Navigate().GoToUrl("https://www.meuip.com.br/");
             chromeDriver.Navigate().GoToUrl("https://www.meuip.com.br/");
